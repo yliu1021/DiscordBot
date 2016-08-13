d096537b426e5782fe72ebab6bcf93d1218afa6c
Yuhan Liu <yliu1021@hotmail.com>
2016-08-13 11:07:57 -0400

    Added Discord api

--- src/com/Yuhan/Main.java
+++ src/com/Yuhan/Main.java
@@ -1,8 +1,15 @@
 package com.Yuhan;
 
+import sx.blah.discord.api.*;
+import sx.blah.discord.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static String TOKEN = "MjE0MDMxNDQ1NzQyNTgzODA4.CpDITQ.BbQrbt9IA4pn-o9h3oLIS2TjH9A";
+
+    private static IDiscordClient client;
+
+    public static void main(String[] args) throws DiscordException {
+        client = new ClientBuilder().withToken(TOKEN).login();
     }
 }
